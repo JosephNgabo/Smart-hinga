14aa218678387edfdb108c04827b68be66c4cfa9
JosephNgabo <ntwarijoseph2050@gmail.com>
2024-01-23 09:08:29 +0200

    Creation of Reusable Navigation bar and some Hero components

---
+++ Desktop/SMART HINGA FE/smart-hinga/components/Button.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+type ButtonProps = {
+    type: string,
+    title: string,
+    icon?: string,
+    variant: 'btn_dark_green'
+    }
+
+const Button = ({type, title, icon, variant}: ButtonProps) => {
+  return (
+    <button
+    className={`flexCenter gap-3 rounded-full ${variant}`}
+    type={type}
+    >
+    {icon && <Image src={icon} alt={title} width={20} height={20} />}
+    <label className="bold-16 whitespace-nowrap">{title}</label>
+    </button>
+  )
+}
+
+export default Button
